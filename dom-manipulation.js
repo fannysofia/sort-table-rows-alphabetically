f43e7ac77f8afc879be5ab1f01a7e18ea1cdf02c
Fanny T <38237496+fannysofia@users.noreply.github.com>
2021-01-27 18:10:39 +0200

    Add files via upload

---
+++ dom-manipulation.js
@@ -0,0 +1,75 @@
+/**
+ * Sort table rows alphabetically based on the values in a column
+ *
+ * @param {Number} col column index (zero based)
+ * @param {HTMLTableElement} table the table to be sorted
+ */
+function sortTableByColumn(col, table) {
+  // TODO: Implement this function
+  var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  //table = document.getElementById("myTable2");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[col];
+      y = rows[i + 1].getElementsByTagName("TD")[col];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+
+      }
+  
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+  };
+
+/**
+ * DO NOT EDIT THE CODE BELOW!
+ *
+ * The code below is there just to make it easier to test the code.
+ *
+ * If your function works correctly you should be able to sort the table
+ * simply by clicking any column heading and the table should be immediately
+ * sorted by values in that column.
+ */
+
+// find the table element
+const table = document.getElementById('sortable');
+
+// attach an event listener to each th element's click event
+table.querySelectorAll('thead th').forEach((th, i) =>
+  th.addEventListener('click', () => {
+    sortTableByColumn(i, table);
+  })
+);
